ba929f0b0493b1bd202fe0f5cb2b88dfea913b47
magical-builder-5600 <gabbinguyen@gmail.com>
2020-09-21 18:07:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function takeANumber (katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 // .push() adds name to the end of the array (i.e. end of the customer line)
+// .length property is used to identify the number of elements in the array
+// template literal placeholder is used and is replacd by the new values (name of the customer and the new current line)
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length > 0) {
@@ -16,13 +18,15 @@ function nowServing (katzDeliLine) {
 // if/else identifies if there is a line or not and uses the correct statement for either scenario
 
 function currentLine(katzDeliLine) {
-  var lineNum = [];
-  if (katzDeliLine.length === 0) {
-    return `The line is currently empty.`
+  var line = []
+  if (katzDeliLine.length > 0) {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      line.push(` ${i+1}. ${katzDeliLine[i]}`);
+    } return `The line is currently:${line}`
   } else {
-      for (var i = 0; i < katzDeliLine.length; i++) {
-        lineNum.push(` ${i + 1}. ${katzDeliLine[i]}`)
-  } return `The line is currently:${lineNum}`;
+      return `The line is currently empty.`
   }
 }
-// for loop iterates through the array (the line of customers), and outputs each element in the array (customer names)
+// for loop iterates through katzDeliLine
+// second array "line"  -- EXPLAIN MAYBE LOOK UP MULTIDEMINSIONAL ARRAYS??
+// if/else identifies if there is a line or not and uses the correct statement for either scenario
